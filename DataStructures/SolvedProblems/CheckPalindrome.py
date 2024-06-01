46698a6c23cdce5e9a13024c61b262c4a6448a5e
Nithish Kumar S <145916416+Nithish130718@users.noreply.github.com>
2024-06-01 19:30:48 +0530

    Update CheckPalindrome.py

--- DataStructures/SolvedProblems/CheckPalindrome.py
+++ DataStructures/SolvedProblems/CheckPalindrome.py
@@ -1,26 +1,76 @@
-"""
-Question : To check if a given number is a palindrome using a stack and queue, given that you're allowed to use the list-based implementations of the same.
+
 """
 
-'''
-You can find the link-based implementation of the stack [here](https://github.com/Nithish130718/DSA/new/main/DataStructures/Stacks/List_Implentation.py) and queue [here](https://github.com/Nithish130718/DSA/new/main/DataStructures/Queues/List_Implentation.py)
-'''
+Question: To check if a given number is a palindrome or not using an empty stack, queue and their related functions. 
+HINT: You are allowed to use list based implementation for the same.
+
+"""
+
+class Stack:
+    '''
+    This class is an implementation of the stack ADT.
+    '''
+    def __init__(self):
+        self.item = []
+    def isempty(self):
+        if len(self.item) == 0:
+            return True
+        return False
+    def push(self,ele):
+        self.item.append(ele)
+    def pop(self):
+        ele = self.item[-1]
+        self.item.pop()
+        return str(ele)
+    def __len__(self):
+        return len(self.item)
+    def __str__(self):
+        return str(self.item) 
+    def top_ele(self):#peek/peep 
+        return self.item[-1]
+    
+
+class Queue:
+    '''
+    This class is an implementation f '''
+    def __init__(self):
+        self.item = []
+    def isempty(self):
+        if len(self.item) == 0:
+            return True
+        return False
+    def enqueue(self,ele):
+        self.item.append(ele)
+    def dequeue(self):
+        ele = self.item[0]
+        self.item.pop(0)
+        return str(ele)
+    def __len__(self):
+        return len(self.item)
+    def __str__(self):
+        return str(self.item) 
+    def first_ele(self):
+        return self.item[0]
+
+    
 
 if __name__ == '__main__':
-    # Checking if a number is a palindrome.
+    
+    #Checking if a number is a palindrome.
     n = input("Enter a number:")
     def palindrome(n):
         s1 = Stack()
         q1 = Queue()
-        initial_n, reversed_n = '', ''
+        initial_n, reversed_n = '',''
         for i in n:
             s1.push(int(i))
             q1.enqueue(int(i))
         for i in range(len(n)):
-            initial_n += str(q1.dequeue())
-            reversed_n += str(s1.pop())
+            initial_n +=  q1.dequeue()
+            reversed_n += s1.pop() 
+        
         if initial_n == reversed_n:
-            return 'Yes, it\'s a palindrome'
+            return 'Yes, its a palindrome'
         else:
-            return 'No, it\'s not a palindrome.'
+            return 'No, its not a palindrome.'   
     print(palindrome(n))
