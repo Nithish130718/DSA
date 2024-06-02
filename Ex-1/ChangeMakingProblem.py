2a24d6b3f6b9501c37112cb1896a7659443f54c3
Nithish Kumar S <145916416+Nithish130718@users.noreply.github.com>
2024-06-02 10:39:24 +0530

    Update ChangeMakingProblem.py

--- Ex-1/ChangeMakingProblem.py
+++ Ex-1/ChangeMakingProblem.py
@@ -10,58 +10,67 @@ It involves determining the minimum number of currency notes (or coins) needed t
 The goal is to provide the change in such a way that it is optimal, meaning it uses the least number of currency denominations possible.
 """
 
-
 def balance(bill, given):
-
     change = given - bill
+    res = ""
 
-    str = ""
+    if change >= 2000:
+        num = change // 2000
+        res += f"{num} two thousand rupee note{'s' if num > 1 else ''}\n"
+        change = change % 2000
 
-    if change % 2000 > 0:
-        if change % 2000 == 1:
-            str += "1 two thousand rupee note"
-            change = change - change // 2000 * 2000
-        elif change % 2000 > 1:
-            str += str(change % 2000) + "two thousand rupee note"
-            change = change - change // 2000 * 2000
+    if change >= 500:
+        num = change // 500
+        res += f"{num} five hundred rupee note{'s' if num > 1 else ''}\n"
+        change = change % 500
 
-    if change % 500 > 0:
-        if change % 500 == 1:
-            str += "1 five hundred rupee note"
-            change = change - change // 500 * 500
-        elif change % 500 > 1:
-            str += str(change % 2000) + "five hundred rupee notes"
-            change = change - change // 500 * 500
+    if change >= 200:
+        num = change // 200
+        res += f"{num} two hundred rupee note{'s' if num > 1 else ''}\n"
+        change = change % 200
 
-    if change % 200 > 0:
-        if change % 500 == 1:
-            str += "1 two hundred rupee note"
-            change = change - change // 200 * 200
-        elif change % 500 > 1:
-            str += str(change % 2000) + "two hundred rupee notes"
-            change = change - change // 200 * 200
+    if change >= 100:
+        num = change // 100
+        res += f"{num} hundred rupee note{'s' if num > 1 else ''}\n"
+        change = change % 100
 
-    if change % 100 > 0:
-        if change % 100 == 1:
-            str += "1 hundred rupee note"
-            change = change - change // 100 * 100
-        elif change % 100 > 1:
-            str += str(change % 2000) + "hundred rupee notes"
-            change = change - change // 100 * 100
+    if change >= 50:
+        num = change // 50
+        res += f"{num} fifty rupee note{'s' if num > 1 else ''}\n"
+        change = change % 50
 
-    if change % 50 > 0:
-        if change % 50 == 1:
-            str += "1 fifty rupee note"
-            change = change - change // 50 * 50
-        elif change % 500 > 1:
-            str += str(change % 2000) + "fifty rupee notes"
-            change = change - change // 50 * 50
+    if change >= 20:
+        num = change // 20
+        res += f"{num} twenty rupee note{'s' if num > 1 else ''}\n"
+        change = change % 20
 
-    print(str)
+    if change >= 10:
+        num = change // 10
+        res += f"{num} ten rupee note{'s' if num > 1 else ''}\n"
+        change = change % 10
+
+    if change >= 5:
+        num = change // 5
+        res += f"{num} five rupee coin{'s' if num > 1 else ''}\n"
+        change = change % 5
+
+    if change >= 2:
+        num = change // 2
+        res += f"{num} two rupee coin{'s' if num > 1 else ''}\n"
+        change = change % 2
+
+    if change >= 1:
+        num = change // 1
+        res += f"{num} one rupee coin{'s' if num > 1 else ''}\n"
+        change = change % 1
+
+    print(res)
 
 
-bill = int(input("Enter the bill amount:"))
-given = int(input("Enter the given amount by the user:"))
+bill = int(input("Enter the bill amount: "))
+given = int(input("Enter the given amount by the user: "))
 
 if bill < given:
     balance(bill, given)
+else:
+    print("The given amount should be greater than the bill amount.")
